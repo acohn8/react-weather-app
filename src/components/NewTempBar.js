3fba73b75e55d4cbbf5b0beb9eb055a481dab5d2
Adam Cohn <adamcohn88@gmail.com>
2018-07-12 22:16:03 -0400

    fancy new chart

--- src/components/NewTempBar.js
+++ src/components/NewTempBar.js
@@ -1,27 +1,102 @@
-import { VictoryBar, VictoryChart, VictoryTooltip } from 'victory';
+import {
+  VictoryBar,
+  VictoryChart,
+  VictoryTooltip,
+  VictoryLine,
+  createContainer,
+  VictoryBrushContainer,
+  VictoryAxis,
+  VictoryTheme,
+} from 'victory';
 import React from 'react';
 
-const TempVBar = ({ forecast }) => {
-  const data = forecast.map(el => ({
-    date: el.time * 1000,
-    temperature: el.temperature,
-    label: `${Math.round(el.temperature)}°`,
-  }));
-  return (
-    <VictoryChart scale={{ x: 'time' }} domainPadding={5}>
-      <VictoryBar
-        labelComponent={<VictoryTooltip />}
-        data={data}
-        x="date"
-        y="temperature"
-        animate={{
-          duration: 2000,
-          onLoad: { duration: 1000 },
-        }}
-        cornerRadius={3}
-        style={{ data: { fill: 'orange' } }}
-      />
-    </VictoryChart>
-  );
-};
+class TempVBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  handleZoom = domain => {
+    this.setState({ selectedDomain: domain });
+  };
+
+  handleBrush = domain => {
+    this.setState({ zoomDomain: domain });
+  };
+
+  render() {
+    const VictoryZoomVoronoiContainer = createContainer('zoom', 'voronoi');
+    const tempData = this.props.forecast.map(el => ({
+      x: el.time * 1000,
+      y: el.temperature,
+      l: `Temperature: ${Math.round(el.temperature)}°`,
+    }));
+    const precipData = this.props.forecast.map(el => ({
+      x: el.time * 1000,
+      y: Math.round(el.precipProbability * 100),
+      l: `Precipitation: ${Math.round(el.precipProbability * 100)}%`,
+    }));
+    return (
+      <div>
+        <VictoryChart
+          width={700}
+          height={200}
+          scale={{ x: 'time' }}
+          theme={VictoryTheme.material}
+          padding={{ top: 0, left: 50, right: 50, bottom: 35 }}
+          containerComponent={
+            <VictoryZoomVoronoiContainer
+              domain={{ y: [0, 110] }}
+              zoomDimension="x"
+              zoomDomain={this.state.zoomDomain}
+              onZoomDomainChange={this.handleZoom}
+              voronoiDimension="x"
+              labels={d => `${d.l}`}
+              labelComponent={<VictoryTooltip cornerRadius={0} flyoutStyle={{ fill: 'white' }} />}
+            />
+          }
+        >
+          <VictoryLine
+            interpolation="natural"
+            data={tempData}
+            style={{
+              data: { stroke: 'orange' },
+              strokeWidth: 10,
+              strokeLinecap: 'round',
+            }}
+          />
+          <VictoryBar data={precipData} cornerRadius={3} style={{ data: { fill: 'teal' } }} />
+        </VictoryChart>
+
+        <VictoryChart
+          theme={VictoryTheme.material}
+          domain={{ y: [0, 110] }}
+          padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
+          width={700}
+          height={70}
+          scale={{ x: 'time' }}
+          containerComponent={
+            <VictoryBrushContainer
+              brushDimension="x"
+              brushDomain={this.state.selectedDomain}
+              onBrushDomainChange={this.handleBrush}
+            />
+          }
+        >
+          <VictoryAxis />
+          <VictoryLine
+            interpolation="natural"
+            data={tempData}
+            style={{
+              data: { stroke: 'orange' },
+              strokeWidth: 10,
+              strokeLinecap: 'round',
+            }}
+          />
+          <VictoryBar data={precipData} cornerRadius={3} style={{ data: { fill: 'teal' } }} />
+        </VictoryChart>
+      </div>
+    );
+  }
+}
 export default TempVBar;
--- src/components/TodayAndTomorrow.js
+++ src/components/TodayAndTomorrow.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Header, Grid } from 'semantic-ui-react';
-import PrecipBar from './PrecipBar';
-import TempLine from './TempLine';
 import TempVBar from './NewTempBar';
-import PrecipVBar from './NewPercipBar';
 
 const TodayAndTomorrow = ({ forecast }) => (
   <div>
@@ -11,16 +8,10 @@ const TodayAndTomorrow = ({ forecast }) => (
       Today and Tomorrow
       <Header.Subheader>{forecast.summary}</Header.Subheader>
     </Header>
-    <Grid centered stackable columns={2}>
-      <Grid.Column>
-        <Header size="small">Precipitation chance</Header>
-        <PrecipVBar forecast={forecast.data} />
-      </Grid.Column>
-      <Grid.Column>
-        <Header size="small">Temperature </Header>
-        <TempVBar forecast={forecast.data} />
-      </Grid.Column>
-    </Grid>
+    <Header size="small">Temperature & precipitation chance</Header>
+    <Grid.Row style={{ height: '30%' }}>
+      <TempVBar forecast={forecast.data} />
+    </Grid.Row>
   </div>
 );
 
